d6ba384767a15c77cc6bcd22769a0f2b7b76dc65
github username <github email address>
2019-12-25 15:52:22 -0500

    edited new_categories.erb

--- Sinatra Project/app/controllers/users_controller.rb
+++ Sinatra Project/app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
     get "/signup" do
         erb :'users/new'
- end
+    end
 
- post "/" do
-    erb :'users/new'
-end
+    post "/" do
+        erb :'users/new'
+    end
 
 end
