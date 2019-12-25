7d8be072ff468e23411df81a340843df65bed666
github username <github email address>
2019-12-25 18:16:36 -0500

    changed user_controller

--- Sinatra Project/app/controllers/users_controller.rb
+++ Sinatra Project/app/controllers/users_controller.rb
@@ -3,8 +3,15 @@ class UsersController < ApplicationController
         erb :'users/new'
     end
 
-    post "/" do
-        erb :'users/new'
+    post "/users" do
+        puts "got hereee==>>   #{params}"
+        @user = User.all.find_or_create_by(username: params[:username])
+        if @user.password_hash == nil
+            @user.password_hash = password
+            @user.save
+        else
+
+        end
     end
 
 end
--- Sinatra Project/config/environment.rb
+++ Sinatra Project/config/environment.rb
@@ -2,11 +2,12 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 ActiveRecord::Base.logger = Logger.new(STDOUT)
-configure :development do
-  ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-  )
-end
+#configure :development do
+  # set :database, "db/#{ENV['SINATRA_ENV']}.sqlite"
+  # ActiveRecord::Base.establish_connection(
+  #   :adapter => "sqlite3",
+  #   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  # )
+#end
 
 require_all 'app'
