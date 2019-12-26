2e96527bf0966990c4fcd46f7f01a45dd541f1dc
github username <github email address>
2019-12-26 10:14:07 -0500

    updated spec.md

--- Sinatra Project/app/controllers/categories_controller.rb
+++ Sinatra Project/app/controllers/categories_controller.rb
@@ -1,2 +1,22 @@
 class CategoriesController < ApplicationController
+    get '/categories/new' do
+        erb :"categories/new"
+    end   
+
+    post '/categories' do
+        puts "categories here #{params}"
+        category = Category.new(name: params[:name])
+        if category.save!
+            redirect '/categories'
+        else
+            redirect "/categories/new"
+        end
+    end
+
+    get '/categories' do
+        @categories = Category.all 
+        erb :"categories/index"
+        #puts "list of categories==>>>>>  #{@categories.inspect}"
+    end
 end
+
